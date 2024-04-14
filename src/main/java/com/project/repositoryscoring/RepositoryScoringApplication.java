8ac4ea7cae9f096077b93ab21198e6b5369a69d9
Hussein Akar <hak@container-xchange.com>
2024-04-14 07:32:21 +0200

    Initial commit

---
+++ src/main/java/com/project/repositoryscoring/RepositoryScoringApplication.java
@@ -0,0 +1,13 @@
+package com.project.repositoryscoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryScoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryScoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/repositoryscoring/RepositoryScoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.repositoryscoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryScoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
